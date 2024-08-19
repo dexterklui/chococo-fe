d276db0f5e0571cc7e043f6744818056898a37d8
Dexter Lui <dexlui@pm.me>
2024-08-19 21:57:16 +0800

    refactor: ColDef move to CoffeeProductTable

--- src/components/CoffeeProductTable.tsx
+++ src/components/CoffeeProductTable.tsx
@@ -2,6 +2,7 @@
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import { Coffee } from "../lib/data-fetch/coffee";
 
 export type ColDef = {
   field: string;
@@ -9,27 +10,56 @@ export type ColDef = {
   filterParams?: object;
 };
 
-export default function CoffeeProductTable({
-  rowData,
-  colDefs,
-}: {
-  rowData: object[];
-  colDefs: object[];
-}) {
-  rowData = rowData.map((row) => {
-    return {
-      ...row,
-      "avg_rating from customer": row["avg_rating from customer"].toFixed(2),
-      "Price / 100g in HKD": row["Price / 100g in HKD"].toFixed(2),
-    };
-  });
+const defaultColDefs: ColDef[] = [
+  { field: "source_type", filter: true },
+  { field: "name", filter: true },
+  { field: "Taste" },
+  { field: "Variety", filter: true },
+  { field: "Process", filter: true },
+  { field: "Country", filter: true },
+  { field: "Region", filter: true },
+  { field: "Price / 100g in HKD" },
+  { field: "Roast", filter: true },
+  { field: "avg_rating from customer", filter: true },
+  { field: "Altitute in meters", filter: true },
+  { field: "Flavors_Spicy", filter: true },
+  { field: "Flavors_Choclaty", filter: true },
+  { field: "Flavors_Nutty", filter: true },
+  { field: "Flavors_Buttery", filter: true },
+  { field: "Flavors_Fruity", filter: true },
+  { field: "Flavors_Flowery", filter: true },
+  { field: "Flavors_Winey", filter: true },
+  { field: "Flavors_Earthy", filter: true },
+  { field: "Attributes_Brightness", filter: true },
+  { field: "Attributes_Body", filter: true },
+  { field: "Attributes_Aroma", filter: true },
+  { field: "Attributes_Complexity", filter: true },
+  { field: "Attributes_Balance", filter: true },
+  { field: "Attributes_Sweetness", filter: true },
+  { field: "summary_comment" },
+];
 
+export default function CoffeeProductTable({
+  coffees,
+  colDefs = defaultColDefs,
+}: {
+  coffees: Coffee[];
+  colDefs?: ColDef[];
+}) {
   return (
     <div
       className="ag-theme-quartz" // applying the Data Grid theme
       style={{ height: 500, minWidth: "70vw" }} // the Data Grid will fill the size of the parent container
     >
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      <AgGridReact
+        rowData={coffees.map((coffee) => ({
+          ...coffee,
+          "avg_rating from customer":
+            coffee["avg_rating from customer"].toFixed(2),
+          "Price / 100g in HKD": coffee["Price / 100g in HKD"].toFixed(2),
+        }))}
+        columnDefs={colDefs}
+      />
     </div>
   );
 }
--- src/components/CoffeeProducts.tsx
+++ src/components/CoffeeProducts.tsx
@@ -2,42 +2,11 @@ import { useEffect, useState } from "react";
 import { Coffee, getAllCoffee } from "../lib/data-fetch/coffee";
 import ChatbotWidget from "./ChatbotWidget";
 import CoffeeProductsCatalogue from "./CoffeeProductsCatalogue";
-import CoffeeProductTable, { ColDef } from "./CoffeeProductTable";
-
-const defaultColDefs: ColDef[] = [
-  { field: "source_type", filter: true },
-  { field: "name", filter: true },
-  { field: "Taste" },
-  { field: "Variety", filter: true },
-  { field: "Process", filter: true },
-  { field: "Country", filter: true },
-  { field: "Region", filter: true },
-  { field: "Price / 100g in HKD" },
-  { field: "Roast", filter: true },
-  { field: "avg_rating from customer", filter: true },
-  { field: "Altitute in meters", filter: true },
-  { field: "Flavors_Spicy", filter: true },
-  { field: "Flavors_Choclaty", filter: true },
-  { field: "Flavors_Nutty", filter: true },
-  { field: "Flavors_Buttery", filter: true },
-  { field: "Flavors_Fruity", filter: true },
-  { field: "Flavors_Flowery", filter: true },
-  { field: "Flavors_Winey", filter: true },
-  { field: "Flavors_Earthy", filter: true },
-  { field: "Attributes_Brightness", filter: true },
-  { field: "Attributes_Body", filter: true },
-  { field: "Attributes_Aroma", filter: true },
-  { field: "Attributes_Complexity", filter: true },
-  { field: "Attributes_Balance", filter: true },
-  { field: "Attributes_Sweetness", filter: true },
-  { field: "summary_comment" },
-];
+import CoffeeProductTable from "./CoffeeProductTable";
 
 export default function CoffeeProducts() {
   const [coffees, setCoffees] = useState<Coffee[]>([]);
   const [allCoffees, setAllCoffees] = useState<Coffee[]>([]);
-  const colDefs = defaultColDefs;
-  // const [colDefs, setColDefs] = useState<ColDef[]>(defaultColDefs);
   const [tableMode, setTableMode] = useState<boolean>(true);
 
   useEffect(() => {
@@ -61,7 +30,7 @@ export default function CoffeeProducts() {
       </div>
       <section>
         {tableMode ? (
-          <CoffeeProductTable rowData={coffees} colDefs={colDefs} />
+          <CoffeeProductTable coffees={coffees} />
         ) : (
           <CoffeeProductsCatalogue coffees={coffees} />
         )}
